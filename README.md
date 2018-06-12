d2f9444b687e4c2c84b4c539cd697037d224fc4a
hawkgs <hawk.dvp@gmail.com>
2018-06-12 16:06:25 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# photo-to-scan
+
+A simple JavaScript class which converts a document photo to a scan-like image.
+
+Far from perfect. Made for demo purposes.
+
+## API
+
+**`PhotoScan(imageSource[, options])`** - constructor
+
+- `imageSource: string`: Source of the photo
+- `options: { luminanceThreshold: number; grayscale: number; brightness: number }` (optional): Set of options for tuning the output scan
+  - Default options: `luminanceThreshold`: 200, `grayscale`: 1, `brightness`: 1.5
+
+
+**`PhotoScan.getCanvas()`** - returns `HTMLCanvasElement` of the scan
+
+## How it works?
+
+```javascript
+  const scan = new PhotoToScan('./image.jpg');
+  const canvas = scan.getCanvas();
+  ...
+```
+
+## License
+
+MIT
---
+++ scanner.js
@@ -0,0 +1,71 @@
+(function () {
+  'use strict';
+
+  const WHITE = 255;
+  const LUMINANCE_THRESHOLD = 200;
+  const GRAYSCALE = 1;
+  const BRIGHTNESS = 1.5;
+
+  class PhotoToScan {
+    constructor(imageSource, options) {
+      this._canvas = document.createElement('canvas');
+      this._ctx = this._canvas.getContext('2d');
+
+      options = options || {};
+      this._lumaThreshold = options.luminanceThreshold || LUMINANCE_THRESHOLD;
+      this._grayscale = options.grayscale || GRAYSCALE;
+      this._brightness = options.brightness || BRIGHTNESS;
+
+      this._load(imageSource);
+    }
+
+    getCanvas() {
+      return this._canvas;
+    }
+
+    _load(imageSource) {
+      const img = new Image();
+
+      img.onload = () => {
+        this._canvas.width = img.width;
+        this._canvas.height = img.height;
+
+        this._ctx.filter = `grayscale(${this._grayscale}) brightness(${this._brightness})`;
+        this._ctx.drawImage(img, 0, 0, img.width, img.height);
+
+        this._normalizeWhite(img.width, img.height);
+      };
+
+      img.src = imageSource;
+    }
+
+    _normalizeWhite(width, height) {
+      const imgData = this._ctx.getImageData(0, 0, width, height);
+
+      for (let y = 0; y < height; y += 1) {
+        for (let x = 0; x < width; x += 1) {
+          const rgb = this._getRGBIdxs(x, y, width);
+          const luma = this._getLuminance(imgData.data, rgb);
+
+          if (luma >= this._lumaThreshold) {
+            imgData.data[rgb[0]] = imgData.data[rgb[1]] = imgData.data[rgb[2]] = WHITE;
+          }
+        }
+      }
+
+      this._ctx.putImageData(imgData, 0, 0);
+    }
+
+    _getRGBIdxs(x, y, width) {
+      const red = y * (width * 4) + x * 4;
+      return [red, red + 1, red + 2];
+    }
+
+    _getLuminance(data, rgb) {
+      return parseInt(data[rgb[0]] * 0.2126 + data[rgb[1]] * 0.7152 + data[rgb[2]] * 0.0722, 10);
+    }
+  }
+
+  const scan = new PhotoToScan('./image.jpg');
+  document.body.appendChild(scan.getCanvas());
+}());
